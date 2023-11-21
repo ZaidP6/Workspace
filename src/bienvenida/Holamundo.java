85f301626424546ac6cfb0c6b7ec22b9b156d2b6
Pilar <aguilar.dimar23@triana.salesianos.edu>
2023-11-21 14:30:21 +0100

    subida 21 nov

---
+++ src/bienvenida/Holamundo.java
@@ -0,0 +1,10 @@
+package bienvenida;
+
+public class Holamundo {
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenido a la clase de entornos");
+		
+		System.out.println("Segundo comentario");
+	}
+}
