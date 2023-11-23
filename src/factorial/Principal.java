f29b8dfc1c38a84dfc92299842d5126c897b45d8
Pilar <aguilar.dimar23@triana.salesianos.edu>
2023-11-23 14:31:03 +0100

    23 nov 14.30

---
+++ src/factorial/Principal.java
@@ -0,0 +1,20 @@
+package factorial;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("El factorial de 5 es : "+factorial(5));
+		
+	}
+	
+	public static int factorial(int n) {
+		int resultado = 1;
+		for (int i = n; i>=1 ; i--) {
+			resultado = resultado *1;
+		}
+		return resultado;
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module SaludoED {
+module Factorial {
 }
